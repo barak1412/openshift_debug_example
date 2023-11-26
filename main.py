6d18430d634bbdf4bdc670498f26a2620aa6f5b8
barak1412 <barak1413@gmail.com>
2023-11-26 08:17:51 +0200

    test

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import time
+import logging
 
+
+stdout_handler = logging.StreamHandler()
+stdout_handler.setLevel(logging.INFO)
+logging.getLogger().addHandler(stdout_handler)
+
+logging.info('loading...')
 print('loading...')
 time.sleep(60)
+logging.info('yea!')
 print('yea!')
 time.sleep(60)
